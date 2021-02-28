b867e4d430d5ebf1142ed29704f43ada98db758f
Jeremy Buentello <jb09056@georgiasouthern.edu>
2021-02-27 21:33:47 -0500

    closes #1 Adding all of the classes for the traps and such

---
+++ Assets/Scripts/Interactables/Teleporter.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class Teleporter : BaseInteractable
+{
+    [Tooltip("The position to teleport the player to")]
+    public Transform TeleportLocation;
+
+    [Tooltip("If the player needs to stay on the teleporter, set to true")]
+    public bool IsDelayTeleport = false;
+
+    [Tooltip("The amount of time the player needs to stand on the teleporter")]
+    public float Delay = 1f;
+
+    private float delayTimer = 0;
+
+    protected override void OnPlayerTriggerEnter(Player player)
+    {
+        delayTimer = 0;
+        if (!IsDelayTeleport)
+            TeleportPlayer(player);
+    }
+
+    protected override void OnPlayerTriggerExit(Player player)
+    {
+        delayTimer = 0;
+    }
+
+    protected override void OnPlayerTriggerStay(Player player)
+    {
+        delayTimer += Time.deltaTime;
+        if (delayTimer > Delay)
+            TeleportPlayer(player);
+    }
+
+    private void TeleportPlayer(Player player)
+    {
+        player.transform.position = TeleportLocation.position;
+    }
+
+    private void Start()
+    {
+        if (TeleportLocation == null)
+            Debug.LogError("The teleporter needs to have a teleport location assigned to function correctly", this);
+    }
+}
---
+++ Assets/Scripts/Interactables/WindTurbine.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class WindTurbine : BaseInteractable
+{
+    [Tooltip("Will apply this amount of force in the direction to the player")]
+    public Vector3 Force;
+
+    private Rigidbody playerRigidBody;
+
+    protected override void OnPlayerTriggerEnter(Player player)
+    {
+        if (playerRigidBody == null)
+            playerRigidBody = player.GetComponent<Rigidbody>();
+    }
+
+    protected override void OnPlayerTriggerExit(Player player)
+    {
+        return;
+    }
+
+    protected override void OnPlayerTriggerStay(Player player)
+    {
+        playerRigidBody.AddForce(Force, ForceMode.Force);
+    }
+}
