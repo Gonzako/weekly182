2513ed8fc5d451c45f828ccae9ec3d2e568770c3
Gonzako <gonzako123@gmail.com>
2021-03-01 17:59:44 +0100

    Edited player physics added wincircle prefab + model

---
+++ Assets/Scripts/Interactables/GoalInteractible.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// 
+/// Copyright (c) 2021 All Rights Reserved
+///
+/// <author>Gonzako</author>
+/// <co-authors>... </co-author>
+/// <summary> Monobeavior class that does something </summary>
+
+
+public class GoalInteractible : BaseInteractable<Player>
+{
+
+    #region PublicFields
+
+    #endregion
+
+    #region PrivateFields
+
+    #endregion
+
+    #region UnityCallBacks
+
+    void Awake()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    void OnEnable()
+    {
+
+    }
+
+
+    private void OnDisable()
+    {
+
+    }
+
+    protected override void OnPlayerTriggerEnter(Player other)
+    {
+
+    }
+
+    protected override void OnPlayerTriggerStay(Player other)
+    {
+        
+    }
+
+    protected override void OnPlayerTriggerExit(Player other)
+    {
+
+    }
+
+
+    #endregion
+
+    #region PublicMethods
+
+    #endregion
+
+
+    #region PrivateMethods
+
+
+    #endregion
+}
