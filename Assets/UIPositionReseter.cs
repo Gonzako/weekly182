86bf07b213451d6b5cd01387b83c0a0a0f74cef7
Gonzako <gonzako123@gmail.com>
2021-03-02 21:20:36 +0100

    Updated MM

---
+++ Assets/UIPositionReseter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// 
+/// Copyright (c) 2021 All Rights Reserved
+///
+/// <author>Gonzako</author>
+/// <co-authors>... </co-author>
+/// <summary> Monobeavior class that does something </summary>
+
+
+public class UIPositionReseter : MonoBehaviour
+{
+
+    #region PublicFields
+    public Transform positionToPutOn;
+    #endregion
+
+    #region PrivateFields
+
+    #endregion
+
+    #region UnityCallBacks
+
+    void Awake()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    void OnEnable()
+    {
+
+    }
+
+
+    private void OnDisable()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.attachedRigidbody.velocity = Vector3.zero;
+        other.transform.position = positionToPutOn.position;
+    }
+
+    #endregion
+
+    #region PublicMethods
+
+    #endregion
+
+
+    #region PrivateMethods
+
+
+    #endregion
+}
