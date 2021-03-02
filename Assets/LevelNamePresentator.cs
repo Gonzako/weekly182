de92354047c69a031392bec54766a1df86cdd254
Gonzako <gonzako123@gmail.com>
2021-03-02 20:50:39 +0100

    Added level presentator

---
+++ Assets/LevelNamePresentator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+/// 
+/// Copyright (c) 2021 All Rights Reserved
+///
+/// <author>Gonzako</author>
+/// <co-authors>... </co-author>
+/// <summary> Monobeavior class that does something </summary>
+
+
+public class LevelNamePresentator : MonoBehaviour
+{
+
+    #region PublicFields
+
+    #endregion
+
+    #region PrivateFields
+    [SerializeField]
+    List<string> levelNames;
+    #endregion
+
+    #region UnityCallBacks
+
+    void Awake()
+    {
+        var i = SceneManager.GetActiveScene().buildIndex;
+        GetComponent<TextMeshProUGUI>().text = levelNames[i];
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    void OnEnable()
+    {
+
+    }
+
+
+    private void OnDisable()
+    {
+
+    }
+
+
+    #endregion
+
+    #region PublicMethods
+
+    #endregion
+
+
+    #region PrivateMethods
+
+
+    #endregion
+}
