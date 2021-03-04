802738946649aa76b222a107307578eac27c0d6f
Gonzako <gonzako123@gmail.com>
2021-03-04 10:21:04 +0100

    Improved main menu and mouse issues

---
+++ Assets/mouseLocker.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// 
+/// Copyright (c) 2021 All Rights Reserved
+///
+/// <author>Gonzako</author>
+/// <co-authors>... </co-author>
+/// <summary> Monobeavior class that does something </summary>
+
+
+public class mouseLocker : MonoBehaviour
+{
+
+    #region PublicFields
+    public bool lockOnClick = true;
+    #endregion
+
+    #region PrivateFields
+
+    #endregion
+
+    #region UnityCallBacks
+
+    void Awake()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    void OnEnable()
+    {
+
+    }
+
+
+    private void OnDisable()
+    {
+
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && lockOnClick)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    #endregion
+
+    #region PublicMethods
+
+
+    public void lockMouse()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    #endregion
+
+
+    #region PrivateMethods
+
+
+    #endregion
+}
